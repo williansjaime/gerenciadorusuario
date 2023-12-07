65e0fd7e38fab36119aa15c774b967a84b6ad86e
williansjaime <williansjaime@outlook.com>
2023-12-06 22:10:29 -0300

    Projeto configurado

---
+++ FrontEnd/gerenciadorusuarios/src/app/template/Adicionar/adicionar/adicionar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar',
+  standalone: true,
+  imports: [],
+  templateUrl: './adicionar.component.html',
+  styleUrl: './adicionar.component.css'
+})
+export class AdicionarComponent {
+
+}
